034fcc86f091217e0c97e8913fe528d9de3b6037
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-11-19 11:55:56 -0600

    added project files

---
+++ Bow Arrow Depd/Assets/Scripts/Arrow.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+
+
+public class Arrow : MonoBehaviour
+{
+
+    SteamVR_TrackedObject trackedObj;
+    private bool isAttached = false;
+    private bool isFired = false;
+
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void OnTriggerStay()
+    {
+        AttachArrow();
+    }
+    void OnTriggerEnter()
+    {
+        AttachArrow();
+    }
+
+    void Update()
+    {
+        if(isFired)
+        {
+            transform.LookAt(transform.position + transform.GetComponent<Rigidbody>().velocity);
+        }  
+    }
+
+    public void Fired()
+    {
+        isFired = true;
+    }
+
+    private void AttachArrow()
+    {
+        var device = SteamVR_Controller.Input((int)ArrowManager.Instance.trackedObj.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            ArrowManager.Instance.AttachBowToArrow();
+            isAttached = true;
+
+        }
+
+    }
+
+}
+
+
---
+++ Bow Arrow Depd/Assets/Scripts/ArrowManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ArrowManager : MonoBehaviour
+{
+    public static ArrowManager Instance;
+
+    private GameObject currentArrow;
+    public GameObject stringAttachPoint;
+    public GameObject arrowStartPoint;
+    public GameObject arrowPrefab;
+    public GameObject stringStartPoint;
+
+    public SteamVR_TrackedObject trackedObj;
+
+    private bool isAttached = false;
+
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+    void OnDestroy()
+    {
+        if (Instance == this)
+            Instance = null;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        AttachArrow();
+        PullString();
+
+    }
+
+    private void AttachArrow()
+    {
+        if (currentArrow == null)
+        {
+            // default arrow start position: (-.08, -2.32, 0.45)
+            currentArrow = Instantiate(arrowPrefab);
+            currentArrow.transform.parent = trackedObj.transform;
+            //currentArrow.transform.localRotation = new Quaternion(0, 0, 0, 0);
+            //currentArrow.transform.localPosition = new Vector3(0f, 0f, .242f);
+            currentArrow.transform.localPosition = new Vector3(0f, 0f, .342f);
+            currentArrow.transform.localRotation = Quaternion.identity;
+
+        }
+    }
+
+    private void PullString()
+    {
+       if (isAttached) {
+			float dist = (stringStartPoint.transform.position - trackedObj.transform.position).magnitude;
+			stringAttachPoint.transform.localPosition = stringStartPoint.transform.localPosition  + new Vector3 (0f, -5f * dist, 0f);
+
+			var device = SteamVR_Controller.Input((int)trackedObj.index);
+			if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)) {
+				Fire();
+			}
+		}
+    }
+
+    private void Fire()
+    {
+        float dist = (stringStartPoint.transform.position - trackedObj.transform.position).magnitude;
+        currentArrow.transform.parent = null;
+        currentArrow.GetComponent<Arrow>().Fired();
+
+        Rigidbody r = currentArrow.GetComponent<Rigidbody>();
+        r.velocity = currentArrow.transform.forward * 25f * dist;
+        r.useGravity = true;
+
+        stringAttachPoint.transform.position = stringStartPoint.transform.position;
+        currentArrow = null;
+        isAttached = false;
+    }
+
+    public void AttachBowToArrow()
+    {
+        currentArrow.transform.parent = stringAttachPoint.transform;
+        currentArrow.transform.position = arrowStartPoint.transform.position;
+        currentArrow.transform.rotation = arrowStartPoint.transform.rotation;
+
+        isAttached = true;
+
+    }
+}
+
